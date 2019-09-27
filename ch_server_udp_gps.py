36c849f08966440256e66ae8ef20bd465360d6c5
Kubra <kubralemdar89@gmail.com>
2019-09-26 23:13:53 -0400

    added control servo part

--- ch_server_udp_gps.py
+++ ch_server_udp_gps.py
@@ -5,64 +5,77 @@ import struct
 # import statistics
 import math
 import gps_service
-
-# Create UDP socket 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('', 50001)
-sock.bind(server_address)
-# sock.setblocking(0)
-print >>sys.stderr, 'starting up on %s port %s' % server_address  # Default print
+import controlServo
 
 
-d=dict()
-while True:
-    #var1 = time.time()
-    data, address = sock.recvfrom(1024)
+def startMain()
+	pin_El, pin_Az = 8,9
 
-    if data:
-        #print "Received data: %s" % data.encode('hex_codec')
-        print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-        #print "Received data: %s" % data.encode('hex_codec')
-        print "Received data: %s" % data
+	# Create UDP socket 
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	server_address = ('', 50001)
+	sock.bind(server_address)
+	boards = controlServo.setupArduino(pin_El, pin_Az)
 
-        if data=="\n":
-            if d.get('Fix')=='1':
-                print("GPS locked")
-		pt = gps_service.GPSPoint(
-			float(d.get("Location")[0]),
-			float(d.get("Location")[1]),
-			float(d.get("Altitude"))
-		)
+	# sock.setblocking(0)
+	print >>sys.stderr, 'starting up on %s port %s' % server_address  # Default print
 
-                #do calculation
-		b_angles = dict()
-		e_angles = dict()
-		for jAntenna in gps_service.JAMMING_ANTENNAS.keys():
+
+	d=dict()
+	while True:
+	    #var1 = time.time()
+	    data, address = sock.recvfrom(1024)
+
+	    if data:
+		#print "Received data: %s" % data.encode('hex_codec')
+		print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+		#print "Received data: %s" % data.encode('hex_codec')
+		print "Received data: %s" % data
+
+		if data=="\n":
+		    if d.get('Fix')=='1':
+		        print("GPS locked")
+			pt = gps_service.GPSPoint(
+				float(d.get("Location")[0]),
+				float(d.get("Location")[1]),
+				float(d.get("Altitude"))
+			)
+
+		        #do calculation
+			b_angles = dict()
+			e_angles = dict()
+			for jAntenna in gps_service.JAMMING_ANTENNAS.keys():
 			
-			b_angles.update(
-				{jAntenna: 
-					gps_service.JAMMING_ANTENNAS[jAntenna].bearingAngle(pt)})
-			e_angles.update(
-				{jAntenna: 
-					gps_service.JAMMING_ANTENNAS[jAntenna].elevationAngle(pt)})
-                d.clear()
-        elif ": " not in data:
-            continue
-        else:
-            line = data.split(': ')
-            key, value = line[0], line[1].strip()
-            if key == 'Location':
-                value = tuple(value.split(', '))
-            d.update({key: value})
-            print "Dict: ", d
-          
-    else:
-        print >> sys.stderr, 'Done', address
-        break
-
-
-             
-# Clean up the connection
-sock.close()
+				b_angles.update(
+					{jAntenna: 
+						gps_service.JAMMING_ANTENNAS[jAntenna].bearingAngle(pt)})
+				e_angles.update(
+					{jAntenna: 
+						gps_service.JAMMING_ANTENNAS[jAntenna].elevationAngle(pt)})
+			# write angles to servos
+			i =0
+			keys_b = sorted(b_angles.keys())
+			keys_e = sorted(e_angles.keys())
+			for a in boards:
+				controlServo.setServoAngle(a,pin_El,e_angles[keys_e[i]][0])
+				controlServo.setServoAngle(a,pin_Az,b_angles[keys_b[i]])
+				i +=1
+		        d.clear()
+		elif ": " not in data:
+		    continue
+		else:
+		    line = data.split(': ')
+		    key, value = line[0], line[1].strip()
+		    if key == 'Location':
+		        value = tuple(value.split(', '))
+		    d.update({key: value})
+		    print "Dict: ", d
+		  
+	    else:
+		print >> sys.stderr, 'Done', address
+		break
+		     
+	# Clean up the connection
+	sock.close()
 
 
